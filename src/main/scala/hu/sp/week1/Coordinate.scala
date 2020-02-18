adca5bb09a87c49245c63804a84f881bfdfee233
Zsolt_Szalai <zsoltszalai73@gmail.com>
2020-02-18 17:40:38 +0100

    with fold, cleaned up version

--- src/main/scala/hu/sp/week1/Coordinate.scala
+++ src/main/scala/hu/sp/week1/Coordinate.scala
@@ -6,16 +6,6 @@ case class Coordinate(x: Int, y: Int) {
     List(Coordinate(x - 1, y), Coordinate(x + 1, y), Coordinate(x, y - 1), Coordinate(x, y + 1))
   }
 
-  def getPassagewayNeighbours = {
-    val leftRight = x % 2 == 0 && y % 2 == 1
-    val upDown = x % 2 == 1 && y % 2 == 0
-    assert(!upDown || !leftRight, s"Not a passageway coordinate! x=$x y=$y")
-    upDown match {
-      case true => List(Coordinate(x, y - 1), Coordinate(x, y + 1))
-      case false => List(Coordinate(x - 1, y), Coordinate(x + 1, y))
-    }
-  }
-
   def +(c: Coordinate) = {
     Coordinate(x + c.x, y + c.y)
   }
--- src/main/scala/hu/sp/week1/MazeGeneratorFour.scala
+++ src/main/scala/hu/sp/week1/MazeGeneratorFour.scala
@@ -4,69 +4,70 @@ import scala.collection.mutable.ListBuffer
 import scala.language.postfixOps
 import scala.util.Random
 
-case class Fold(var nextKey: Int, var cMap: Map[Coordinate, Int], var pathList: ListBuffer[Coordinate], var wallList: ListBuffer[Coordinate])
+case class FoldHelper(var nextKey: Int, var cMap: Map[Coordinate, Int], var pathList: ListBuffer[Coordinate])
 
 // using fold
 object MazeGeneratorFour extends MazeGenerator {
 
-  override def generate(width: Int, height: Int): Maze = {
-    val walls =
-      for {x <- 0 until width
-           y <- 0 until height
-           if (x % 2 == 0 && y % 2 == 0) || x == 0 || x == width - 1 || y == 0 || y == height -1
-           } yield Coordinate(x, y)
-    val path =
-      for {x <- 1 until width - 1
-           y <- 1 until height - 1
-           if x % 2 == 1 && y % 2 == 1
-           } yield Coordinate(x, y)
+  def generate(width: Int, height: Int): Maze = {
 
-    val toDecide = Random.shuffle(for {x <- 1 until width - 1
-                        y <- 1 until height - 1
-                        if x % 2 + y % 2 == 1
-                        } yield Coordinate(x, y))
+    // this will randomize the maze by shuffling tiles order, the ones which can be path or wall
+    val tilesToDecide = Random.shuffle(for (x <- 1 until width - 1; y <- 1 until height - 1 if x % 2 + y % 2 == 1) yield Coordinate(x, y))
+    // decide tiles by marking core path neighbours
+    val foldResult = tilesToDecide.foldLeft(FoldHelper(0, Map.empty, ListBuffer.empty))(processTilesToDecide)
 
-    val foldResult = toDecide.tail.foldLeft(Fold(0, Map.empty, ListBuffer.empty, ListBuffer.empty))((m, c) => {
-      val neighbours = c.getPassagewayNeighbours
-      val val0 = m.cMap.get(neighbours(0))
-      val val1 = m.cMap.get(neighbours(1))
-
-      (val0, val1) match {
-        case (None, Some(i)) => {
-          m.cMap += neighbours(0) -> i
-          m.pathList += c
-        }
-        case (Some(i), None) => {
-          m.cMap += neighbours(1) -> i
-          m.pathList += c
-        }
-        case (Some(i1), Some(i2)) if i1 == i2 => {
-          m.wallList += c
-        }
-        case (Some(i1), Some(i2)) if i1 != i2 => {
-          m.pathList += c
-          val updated = m.cMap.filter(t => t._2 == i1).map(t => t._1 -> i2)
-          m.cMap ++= updated
-        }
-        case (None, None) => {
-          m.cMap += (neighbours(0) -> m.nextKey, neighbours(1) -> m.nextKey)
-          m.nextKey += 1
-          m.pathList += c
-        }
+    def sortTiles(c: Coordinate) = {
+      c match {
+        case c if foldResult.pathList.contains(c) => c -> Tile.PATH // decided as path
+        case c if (c.x % 2 == 1 && c.y % 2 == 1) => c -> Tile.PATH  // path for sure
+        case Coordinate(1, 0) => c -> Tile.PATH                     // entry + exit
+        //case Coordinate(width - 2, height - 1) => c -> Tile.PATH
+        case _ => c -> Tile.WALL                                    // the rest is wall
       }
-      m
-    })
+    }
 
-    val mapWall = walls.map(_ -> Tile.WALL) toMap
-    val mapWallFold = foldResult.wallList.map(_ -> Tile.WALL)
-    val mapPath = path.map(_ -> Tile.PATH) toMap
-    val mapPathFold = foldResult.pathList.map(_ -> Tile.PATH)
-
-    //val mapToDecide = toDecide.map(_ -> Tile.UNDEFINED) toMap
-
-    Maze(mapWall ++ mapPath ++ mapWallFold ++ mapPathFold, width, height)
+    val allCoords = for (x <- 0 until width; y <- 0 until height) yield Coordinate(x, y)
+    Maze(allCoords.map(sortTiles) toMap, width, height)
 
   }
 
+  private def processTilesToDecide(m: FoldHelper, c: Coordinate) = {
+    val neighbours = getPassagewayNeighbours(c)
+    val val0 = m.cMap.get(neighbours(0))
+    val val1 = m.cMap.get(neighbours(1))
+
+    (val0, val1) match {
+      case (None, Some(i)) => {
+        m.cMap += neighbours(0) -> i
+        m.pathList += c
+      }
+      case (Some(i), None) => {
+        m.cMap += neighbours(1) -> i
+        m.pathList += c
+      }
+      case (Some(i1), Some(i2)) if i1 != i2 => {
+        m.pathList += c
+        val updated = m.cMap.filter(t => t._2 == i1).map(t => t._1 -> i2)
+        m.cMap ++= updated
+      }
+      case (None, None) => {
+        m.cMap += (neighbours(0) -> m.nextKey, neighbours(1) -> m.nextKey)
+        m.nextKey += 1
+        m.pathList += c
+      }
+      case _ => None
+    }
+    m
+  }
+
+  private def getPassagewayNeighbours(c: Coordinate) = {
+    val leftRight = c.x % 2 == 0 && c.y % 2 == 1
+    val upDown = c.x % 2 == 1 && c.y % 2 == 0
+    assert(!upDown || !leftRight, s"Not a passageway coordinate! c.x=$c.x c.y=$c.y")
+    upDown match {
+      case true => List(Coordinate(c.x, c.y - 1), Coordinate(c.x, c.y + 1))
+      case false => List(Coordinate(c.x - 1, c.y), Coordinate(c.x + 1, c.y))
+    }
+  }
 
 }
