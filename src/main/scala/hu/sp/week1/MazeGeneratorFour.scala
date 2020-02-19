9fccc7ffbe9621cc91756509925e2e4fdb5b193b
Zsolt_Szalai <zsoltszalai73@gmail.com>
2020-02-19 13:35:50 +0100

    more refactor

--- src/main/scala/hu/sp/week1/MazeGeneratorFour.scala
+++ src/main/scala/hu/sp/week1/MazeGeneratorFour.scala
@@ -13,23 +13,22 @@ object MazeGeneratorFour extends MazeGenerator {
     val tilesToDecide = Random.shuffle(for (x <- 1 until width - 1; y <- 1 until height - 1 if x % 2 + y % 2 == 1) yield Coordinate(x, y))
     val foldResult = tilesToDecide.foldLeft(FoldHelper(0, Map.empty, ListBuffer.empty))(processTiles)
     val allCoords = for (x <- 0 until width; y <- 0 until height) yield Coordinate(x, y)
-    val mazeMap = allCoords.map(c => sortTiles(c, foldResult.pathList toList)).toMap +
+    val mazeMap = allCoords.map(c => categorize(c, foldResult.pathList toList)).toMap +
       (Coordinate(1,0) -> Tile.PATH, Coordinate(width - 2, height - 1) -> Tile.PATH) // add enter and exit
     Maze(mazeMap, width, height)
   }
 
   private def processTiles(m: FoldHelper, c: Coordinate) = {
-    val neighbours = getPassagewayNeighbours(c)
-    val val0 = m.cMap.get(neighbours(0))
-    val val1 = m.cMap.get(neighbours(1))
+    val (n1, n2) = getPassagewayNeighbours(c)
+    val (val1, val2) = (m.cMap.get(n1), m.cMap.get(n2))
 
-    (val0, val1) match {
+    (val1, val2) match {
       case (None, Some(i)) => {
-        m.cMap += neighbours(0) -> i
+        m.cMap += n1 -> i
         m.pathList += c
       }
       case (Some(i), None) => {
-        m.cMap += neighbours(1) -> i
+        m.cMap += n2 -> i
         m.pathList += c
       }
       case (Some(i1), Some(i2)) if i1 != i2 => {
@@ -38,7 +37,7 @@ object MazeGeneratorFour extends MazeGenerator {
         m.cMap ++= updated
       }
       case (None, None) => {
-        m.cMap += (neighbours(0) -> m.nextKey, neighbours(1) -> m.nextKey)
+        m.cMap += (n1 -> m.nextKey, n2 -> m.nextKey)
         m.nextKey += 1
         m.pathList += c
       }
@@ -52,12 +51,12 @@ object MazeGeneratorFour extends MazeGenerator {
     val upDown = c.x % 2 == 1 && c.y % 2 == 0
     assert(!upDown || !leftRight, s"Not a passageway coordinate! c.x=$c.x c.y=$c.y")
     upDown match {
-      case true => List(Coordinate(c.x, c.y - 1), Coordinate(c.x, c.y + 1))
-      case false => List(Coordinate(c.x - 1, c.y), Coordinate(c.x + 1, c.y))
+      case true => (Coordinate(c.x, c.y - 1), Coordinate(c.x, c.y + 1))
+      case false => (Coordinate(c.x - 1, c.y), Coordinate(c.x + 1, c.y))
     }
   }
 
-  def sortTiles(c: Coordinate, pathList: List[Coordinate]) = {
+  def categorize(c: Coordinate, pathList: List[Coordinate]) = {
     c match {
       case c if pathList.contains(c) => c -> Tile.PATH            // decided as path
       case c if (c.x % 2 == 1 && c.y % 2 == 1) => c -> Tile.PATH  // path for sure
