4bf7d57dbfbe91dac1d713d548a0f4d708f27a29
Zsolt_Szalai <zsoltszalai73@gmail.com>
2020-02-19 11:26:05 +0100

    refactor

--- src/main/scala/hu/sp/week1/MazeGeneratorFour.scala
+++ src/main/scala/hu/sp/week1/MazeGeneratorFour.scala
@@ -10,28 +10,15 @@ case class FoldHelper(var nextKey: Int, var cMap: Map[Coordinate, Int], var path
 object MazeGeneratorFour extends MazeGenerator {
 
   def generate(width: Int, height: Int): Maze = {
-
-    // this will randomize the maze by shuffling tiles order, the ones which can be path or wall
     val tilesToDecide = Random.shuffle(for (x <- 1 until width - 1; y <- 1 until height - 1 if x % 2 + y % 2 == 1) yield Coordinate(x, y))
-    // decide tiles by marking core path neighbours
-    val foldResult = tilesToDecide.foldLeft(FoldHelper(0, Map.empty, ListBuffer.empty))(processTilesToDecide)
-
-    def sortTiles(c: Coordinate) = {
-      c match {
-        case c if foldResult.pathList.contains(c) => c -> Tile.PATH // decided as path
-        case c if (c.x % 2 == 1 && c.y % 2 == 1) => c -> Tile.PATH  // path for sure
-        case Coordinate(1, 0) => c -> Tile.PATH                     // entry + exit
-        //case Coordinate(width - 2, height - 1) => c -> Tile.PATH
-        case _ => c -> Tile.WALL                                    // the rest is wall
-      }
-    }
-
+    val foldResult = tilesToDecide.foldLeft(FoldHelper(0, Map.empty, ListBuffer.empty))(processTiles)
     val allCoords = for (x <- 0 until width; y <- 0 until height) yield Coordinate(x, y)
-    Maze(allCoords.map(sortTiles) toMap, width, height)
-
+    val mazeMap = allCoords.map(c => sortTiles(c, foldResult.pathList toList)).toMap +
+      (Coordinate(1,0) -> Tile.PATH, Coordinate(width - 2, height - 1) -> Tile.PATH) // add enter and exit
+    Maze(mazeMap, width, height)
   }
 
-  private def processTilesToDecide(m: FoldHelper, c: Coordinate) = {
+  private def processTiles(m: FoldHelper, c: Coordinate) = {
     val neighbours = getPassagewayNeighbours(c)
     val val0 = m.cMap.get(neighbours(0))
     val val1 = m.cMap.get(neighbours(1))
@@ -67,6 +54,14 @@ object MazeGeneratorFour extends MazeGenerator {
     upDown match {
       case true => List(Coordinate(c.x, c.y - 1), Coordinate(c.x, c.y + 1))
       case false => List(Coordinate(c.x - 1, c.y), Coordinate(c.x + 1, c.y))
+    }
+  }
+
+  def sortTiles(c: Coordinate, pathList: List[Coordinate]) = {
+    c match {
+      case c if pathList.contains(c) => c -> Tile.PATH            // decided as path
+      case c if (c.x % 2 == 1 && c.y % 2 == 1) => c -> Tile.PATH  // path for sure
+      case _ => c -> Tile.WALL                                    // the rest is wall
     }
   }
 
