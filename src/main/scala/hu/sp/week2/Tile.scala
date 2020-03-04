d368e9579dca406302b5c5b76770846798660655
Zsolt_Szalai <zsoltszalai73@gmail.com>
2020-03-04 10:18:37 +0100

    default val

--- src/main/scala/hu/sp/week2/Tile.scala
+++ src/main/scala/hu/sp/week2/Tile.scala
@@ -2,7 +2,8 @@ package hu.sp.week2
 
 import hu.sp.week2.TileType.TileType
 
-case class Tile(tileType: TileType, distanceMap: Map[Coordinate, Int]) {
+case class Tile(tileType: TileType, var distanceMap: Map[Coordinate, Int] = Map.empty) {
 
   override def toString: String = tileType.toString
+
 }
