31c43417bb82185b9a48376af42756b775bd9843
Zsolt_Szalai <zsoltszalai73@gmail.com>
2020-03-04 10:12:46 +0100

    distance map, refactor

--- src/main/scala/hu/sp/week2/Maze.scala
+++ src/main/scala/hu/sp/week2/Maze.scala
@@ -17,39 +17,3 @@ case class Maze(tileMap: Map[Coordinate, Tile], width: Int, height: Int) {
   }
 }
 
-object MazeApp extends App {
-
-//  val (width, height, numOfPaths) = readParams
-//  run(MazeGeneratorFive, width, height, numOfPaths)
-
-    val m = MazeGeneratorFive
-    println(s"${m.generate(37, 17, 5).asString}")
-
-  private def readParams = {
-    println("Maze generator")
-    println("Enter width and height")
-    print("width=")
-    val width = io.StdIn.readInt()
-    print("height=")
-    val height = io.StdIn.readInt()
-    print("count=")
-    val count = io.StdIn.readInt()
-    assert(width > 2 && width % 2 == 1 && height > 2 && height % 2 == 1 && count > 0, "Width and height have to be uneven number, greater than 2!")
-    (width, height, count)
-  }
-
-  private def run(mazeGen: MazeGenerator, width: Int, height: Int, count: Int) = {
-
-    println(s"${mazeGen.getClass.getSimpleName}\n"+"="*mazeGen.getClass.getSimpleName.length)
-    val times = for (_ <- 1 to count) yield {
-      val t0 = System.nanoTime()
-      mazeGen.generate(width, height, 5)
-      val t1 = System.nanoTime()
-      val diff = (t1 - t0).toDouble / 1000000000
-      println(s"Elapsed time: $diff Sec")
-      diff
-    }
-    println(s"Average time = ${times.sum / times.length}\n")
-  }
-
-}
--- src/main/scala/hu/sp/week2/Tile.scala
+++ src/main/scala/hu/sp/week2/Tile.scala
@@ -2,7 +2,7 @@ package hu.sp.week2
 
 import hu.sp.week2.TileType.TileType
 
-case class Tile(tileType: TileType) {
+case class Tile(tileType: TileType, distanceMap: Map[Coordinate, Int]) {
 
   override def toString: String = tileType.toString
 }
