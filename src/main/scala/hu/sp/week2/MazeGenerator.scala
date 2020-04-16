ced2b09f7d29e87e5eddeed1d6f42d8b5fa8c92f
Zsolt Szalai <ext.zsolt.szalai@ef.com>
2020-04-16 17:16:52 +0200

    delete week2, does not compile with this version

--- src/main/scala/hu/sp/week2/MazeGenerator.scala
+++
@@ -1,6 +0,0 @@
-package hu.sp.week2
-
-trait MazeGenerator {
-  def generate(width: Int, height: Int, numOfPaths: Int, numOfDiamonds: Int) : Maze
-
-}
--- src/main/scala/hu/sp/week2/TileType.scala
+++
@@ -1,9 +0,0 @@
-package hu.sp.week2
-
-object TileType extends Enumeration {
-  type TileType = Value
-  val UNDEFINED = Value("??")
-  val WALL = Value("[]")
-  val PATH = Value(Console.WHITE_B + "  " + Console.RESET)
-  val PATH_WITH_DIAMOND = Value(Console.WHITE_B + Console.BLACK + "<>" + Console.RESET)
-}
